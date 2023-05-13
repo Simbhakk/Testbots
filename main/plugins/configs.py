44dbe0a397a9c5f839943fd16c73aaec60c75944
Simbhakk <127213964+Simbhakk@users.noreply.github.com>
2023-05-13 14:26:16 +0530

    Update configs.py

--- main/plugins/configs.py
+++ main/plugins/configs.py
@@ -8,7 +8,4 @@ class Config(object):
 	BOT_USERNAME = os.environ.get("BOT_USERNAME", '')
 	BOT_OWNER = int(os.environ.get("BOT_OWNER", ''))
 	DATABASE_URL = os.environ.get("DATABASE_URL",'')
-	LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ''))
-	BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "1234567890").split())
-	BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "-1001362659779").split()))
 	
