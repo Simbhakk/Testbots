4237a144fde64c5a0fce141f838e1438a743c693
Simbhakk <127213964+Simbhakk@users.noreply.github.com>
2023-05-13 17:45:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ import os
 
 class Config(object):
 	API_ID = int(os.environ.get("API_ID", ''))	
-        DB_URI = os.environ.get("DATABASE_URL", '')
+        DATABASE_URL = os.environ.get("DATABASE_URL", '')
 	BOT_TOKEN = os.environ.get("BOT_TOKEN", '')
-        DB_NAME = os.environ.get("DATABASE_NAME", '')
+        DATABASE_NAME = os.environ.get("DATABASE_NAME", '')
 	API_HASH = os.environ.get("API_HASH", '')
         
