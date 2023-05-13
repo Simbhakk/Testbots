50f2a5352d1cb58689bd25b50f9ef94544531364
Simbhakk <127213964+Simbhakk@users.noreply.github.com>
2023-05-13 17:28:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,8 @@ import os
 class Config(object):
 
 	API_ID = int(os.environ.get("API_ID", ''))	
-
+        DB_URI = os.environ.get("DATABASE_URL", '')
 	BOT_TOKEN = os.environ.get("BOT_TOKEN", '')
-
-	API_HASH = os.environ.get("API_HASH",'')
-        DB_NAME = os.environ.get("DATABASE_NAME", "filesharexbot")
-	DB_URI = os.environ.get("DATABASE_URL", "")
+        DB_NAME = os.environ.get("DATABASE_NAME", '')
+	API_HASH = os.environ.get("API_HASH", '')
         
