c3747de1bf2d3f56f2febfc64853ef468b5ae56e
Simbhakk <127213964+Simbhakk@users.noreply.github.com>
2023-05-13 15:33:58 +0530

    Update start.py

--- main/plugins/start.py
+++ main/plugins/start.py
@@ -10,10 +10,95 @@ from .. import AUTH
 from .database import Database
 
 
-#from ethon.mystarts import start_srb
+from ethon.mystarts import start_srb
     
 S = '/' + 's' + 't' + 'a' + 'r' + 't'
 
+@Drone.on(events.NewMessage(incoming=True, from_users=AUTH, pattern='/broadcast'))
+
+async def send_text(client: Bot, message: Message):
+
+    if message.reply_to_message:
+
+        query = await full_userbase()
+
+        broadcast_msg = message.reply_to_message
+
+        total = 0
+
+        successful = 0
+
+        blocked = 0
+
+        deleted = 0
+
+        unsuccessful = 0
+
+        
+
+        pls_wait = await message.reply("<i>Broadcasting Message.. This will Take Some Time</i>")
+
+        for chat_id in query:
+
+            try:
+
+                await broadcast_msg.copy(chat_id)
+
+                successful += 1
+
+            except FloodWait as e:
+
+                await asyncio.sleep(e.x)
+
+                await broadcast_msg.copy(chat_id)
+
+                successful += 1
+
+            except UserIsBlocked:
+
+                await del_user(chat_id)
+
+                blocked += 1
+
+            except InputUserDeactivated:
+
+                await del_user(chat_id)
+
+                deleted += 1
+
+            except:
+
+                unsuccessful += 1
+
+                pass
+
+            total += 1
+
+        
+
+        status = f"""<b><u>Broadcast Completed</u>
+
+Total Users: <code>{total}</code>
+
+Successful: <code>{successful}</code>
+
+Blocked Users: <code>{blocked}</code>
+
+Deleted Accounts: <code>{deleted}</code>
+
+Unsuccessful: <code>{unsuccessful}</code></b>"""
+
+        
+
+        return await pls_wait.edit(status)
+
+    else:
+
+        msg = await message.reply(REPLY_ERROR)
+
+        await asyncio.sleep(8)
+
+        await msg.delete()
 
 @Drone.on(events.callbackquery.CallbackQuery(data="set"))
 async def sett(event):    
@@ -52,5 +137,5 @@ async def remt(event):
 @Drone.on(events.NewMessage(incoming=True, pattern=f"{S}"))
 async def start(event):
     text = "Send me Link of any message to clone it here, For private channel message, send invite link first"
-    #await start_srb(event, text)
+    await start_srb(event, text)
     
